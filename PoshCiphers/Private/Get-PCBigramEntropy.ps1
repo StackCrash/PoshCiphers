74e97225d62501c695dce229bf66c8e4e7c4157e
stackcrash <derek@itsecguy.com>
2017-05-02 22:45:41 -0700

    Rename parameter 'Text' to 'Plaintext'

--- PoshCiphers/Private/Get-PCBigramEntropy.ps1
+++ PoshCiphers/Private/Get-PCBigramEntropy.ps1
@@ -7,18 +7,18 @@ Function Get-PCBigramEntropy
         .Description
         Returns the bigram entropy for the supplied text compared to English bigram frequancies.
 
-        .Parameter Text
+        .Parameter Plaintext
         Text to generate entropy based on.
 
         .Example
-        Get-PCBigramEntropy -Text "Example"
+        Get-PCBigramEntropy -Plaintext "Example"
         16.535234171974
     #>
     [CmdletBinding()]
     Param
     (
         [Parameter(Mandatory = $True, Position=0, ValueFromPipeline=$True)]
-        [String] $Text
+        [String] $Plaintext
     )
     Begin
     {
@@ -28,18 +28,18 @@ Function Get-PCBigramEntropy
     Process
     {
         #Remove anything that is not a letter
-        $Text = [Regex]::Replace($Text,'[^a-zA-Z]','').ToUpper()
+        $Plaintext = [Regex]::Replace($Plaintext,'[^a-zA-Z]','').ToUpper()
         #Loop though each pair in the text
-        ForEach ($Start in 0..($Text.Length - 2))
+        ForEach ($Start in 0..($Plaintext.Length - 2))
         {
             #Create the pair to use
-            $Pair = $Text[$Start..($Start + 1)]
+            $Pair = $Plaintext[$Start..($Start + 1)]
             #Calculate the entropy
             $Entropy += $Bigrams[($Pair[0] - 65)][($Pair[1] - 65)]
         }   
     }
     End
     {
-        Return $Entropy
+        Return [Double]$Entropy
     }
 }
